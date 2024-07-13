205f5a15ba7d3f4467c8c0ecb6b5954d78d4e7d7
Behnam Sepehri <behnam.sep@gmail.com>
2024-07-13 17:50:11 +0200

    define usestage with different variables type

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import Button from "./Component/Button/Button";
-import Like from "./Component/Like/Like";
-import ListGroup from "./Component/ListGroup";
-import { FaRegCalendarCheck } from "react-icons/fa";
+import UpdatingObjects from "./Component/TestManagingComponent/UpdatingObjects";
 
 const App = () => {
-  const items = ["Germany", "England", "Sweden"];
-  const onClick = () => {
-    console.log("hey");
-  };
   return (
     <>
-      <ListGroup items={items} title="Cities"></ListGroup>
-      <FaRegCalendarCheck color="red" size={100} />
-      <Button onClicked={onClick}>Click Me</Button>
-      <Like onClick={() => console.log("Clicked!")} />
+      <UpdatingObjects />
     </>
   );
 };
